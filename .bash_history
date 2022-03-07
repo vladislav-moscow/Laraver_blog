a2e878f0166b876de71c7f71b7c63ecc041e79cc
Vladislav <Vladislav_lebedkin@mail.ru>
2022-03-07 14:49:18 +0300

    lesson1 laravel blog

---
+++ .bash_history
@@ -0,0 +1,29 @@
+ls
+cd src
+composer global require laravel/installer
+ls
+cd ..
+ls
+cd src
+ls
+composer create-project --prefer-dist laravel/laravel .
+exit
+cd src
+composer create-project --prefer-dist laravel/laravel .
+exit
+ls\
+exit
+ls
+cd src
+ls
+composer create-project --prefer-dist laravel/laravel .
+s
+ls
+https://github.com/mcamara/laravel-localization
+composer require mcamara/laravel-localization
+exit
+cd src
+php artisan vendor:publish --provider="Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider"
+composer require arrilot/laravel-widgets
+php artisan make:widget RecentNews
+exit
---
+++ src/resources/views/header.blade.php
@@ -0,0 +1,31 @@
+<header class="header" id="header">
+    <nav class="nav container">
+        <a href="#" class="nav__logo">Travel</a>
+
+        <div class="nav__menu" id="nav-menu">
+            <ul class="nav__list">
+                <li class="nav__item">
+                    <a href="#" class="nav__link active-link">Home</a>
+                </li>
+                <li class="nav__item">
+                    <a href="#" class="nav__link">About</a>
+                </li>
+                <li class="nav__item">
+                    <a href="#" class="nav__link">News</a>
+                </li>
+            </ul>
+
+            <div class="nav__dark">
+                <!-- Theme change button -->
+                <span class="change-theme-name">Dark mode</span>
+                <i class="ri-moon-line change-theme" id="theme-button"></i>
+            </div>
+
+            <i class="ri-close-line nav__close" id="nav-close"></i>
+        </div>
+
+        <div class="nav__toggle" id="nav-toggle">
+            <i class="ri-function-line"></i>
+        </div>
+    </nav>
+</header>
